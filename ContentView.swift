614f77d9e7f94c79195f3da0587e2bf7b6539c64
West-Grimm <dapickledill76@gmail.com>
2026-04-13 10:57:09 -0500

    Added basic clicker

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var amount = 0
+    @State var upgradeCount = 1
     var body: some View {
-        Text("Roberto")
-        Text("Dylan")
+        Text("\(amount)")
+        Button {
+            amount += upgradeCount
+        } label: {
+            Circle()
+                .frame(width: 100, height: 100)
+        }
+        Button {
+            upgradeCount += 1
+            amount -= 10
+        } label: {
+            Text("Upgrade")
+            Rectangle()
+                .frame(width: 200, height: 100)
+        }
+
     }
 }
