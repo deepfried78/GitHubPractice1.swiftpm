e619d7f54d38eb17d97ed353769c38d2fe10267d
West-Grimm <dapickledill76@gmail.com>
2026-04-13 11:15:21 -0500

    changed the upgrade buttons

--- ContentView.swift
+++ ContentView.swift
@@ -2,17 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State var amount = 0
-    @State var upgradeCount = 1
+    @State var upgradeCount = 0
     var body: some View {
         Text("\(amount)")
         Button {
-            amount += upgradeCount
+            amount += 1 * upgradeCount
         } label: {
             Circle()
                 .frame(width: 100, height: 100)
                 .foregroundStyle(.red)
         }
         Button {
+            if (amount - 10 < 0)
+            {
+                return
+            }
             upgradeCount += 1
             amount -= 10
         } label: {
@@ -24,6 +28,11 @@ struct ContentView: View {
             amount = 0
         }
         Button {
+            if (amount - 1000 < 0)
+            {
+                return
+            }
+            
             upgradeCount += 100
             amount -= 1000
         } label: {
